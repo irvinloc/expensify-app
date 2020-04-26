c0f9008230903a2dab36b52b89e55aa094d2ddd0
unknown <irvinloc@hotmail.com>
2020-04-26 20:13:20 +0200

    tercer intento produccion

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express= require('express');
 const path= require('path');
 const app=express();
 const publicPath = path.join (__dirname, '..','public');
+const port = procees.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.get('*', (req, res)=> {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
-app.listen(3000,()=>{
-    console.log('server listening in port 3000');
+app.listen(port,()=>{
+    console.log('server listening in port ' + port);
 });
