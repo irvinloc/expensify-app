0995ec80db374615fba8e962e7904f641c9277f2
unknown <irvinloc@hotmail.com>
2020-04-26 22:17:48 +0200

    amount and date formatting

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,7 +4,7 @@ import getVisibleExpenses from '../selectors/expenses';
 import ExpenseListItem from './ExpenseListItem';
  
 export const ExpenseList = (props) => (
-    <div>cw
+    <div>
         {props.expenses.length===0 ? (<p>Please, register some expenses</p>):
         (props.expenses.map(x=> <ExpenseListItem  key={x.id} {...x} />))}
     </div>
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,18 @@ import moment from 'moment';
 import { connect } from 'react-redux';
 import React from 'react';
 import { Link} from 'react-router-dom';
+import numeral from 'numeral';
 
 import { removeExpense } from '../actions/expenses';
 
 export const ExpenseListItem = ({dispatch, description, amount, createdAt, id}) => (
     <div>
     
-         <span><Link  to={`/edit/${id}`} >{description} </Link> - {amount/100} -  {moment(createdAt).format("YYYY-MM-DD")} &nbsp;
+         <span><Link  to={`/edit/${id}`} >
+         {description} 
+         </Link> 
+         - {numeral(amount/100).format('$0,0.00').replace('$', '€')} -  
+         {moment(createdAt).format("MMMM Do, YYYY")} &nbsp;
          <button onClick={(e) => {dispatch(removeExpense({id}));} }>Remove</button> 
          </span>
     </div>
